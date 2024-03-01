d7154e320913adf5baba154c2acc26554eb02c9f
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2024-03-02 00:28:44 +0700

    Gem theme added to Gemfile

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709311669',resources: [
+    cacheName: 'chirpy-1709313836',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
